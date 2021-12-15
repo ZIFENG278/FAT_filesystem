415cb32d630b018633a8c63a0a9ca60bbbed6c2e
Zifeng ZHANG <zhang_z9@auditeur.cnam.fr>
2021-12-15 21:28:57 +0100

    i write a main function first, open the OSCAFS first,i use enum to name the integer and have a pointer array which use to match in strcmp() then let user input the operation, then would use strcmp to match in for loop if success to match the OSCAFS function, use switch case to work each funtion(write, read, copy which i don't finsh yet), if fail would tell the user input again , close OSCAFS at the end

---
+++ oscafs.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "osca.h"
+#define LEN 10
+char *s_gets(char *st, int n);
+
+enum os_function
+{
+    write,
+    read,
+    copy
+};
+const char *function_name[] = {"write", "read", "copy"};
+
+int main(void)
+{
+    char choice[LEN];
+    enum os_function operation;
+    bool choice_is_found = false;
+
+    FILE *fp = open_fs("OSCAFS");
+
+    puts("Enter a operation please (empty line to quit) :");
+    while (s_gets(choice, LEN) != NULL && choice[0] != '\0')
+    {
+        for (operation = write; operation <= copy; operation++)
+        {
+            if (strcmp(choice, function_name[operation]) == 0)
+            {
+                choice_is_found = ture;
+                break;
+            }
+        }
+
+        if (choice_is_found)
+            switch (operation)
+            {
+            case write:
+                fs_write();
+                break;
+
+            case read:
+                fs_read();
+                break;
+
+            case copy:
+                fs_copy() break;
+            }
+
+        else
+            printf("OSCA FS haven't %s operation", choice);
+
+        choice_is_found = false;
+        puts("Enter next operation (empty line to quit) :");
+    }
+
+    close_fs(fp);
+    puts("OSCA FS close");
+
+    return 0;
+}
+
+char *s_gets(char *st, int n)
+{
+    char *ret_val;
+    char *find;
+
+    ret_val = fgets(st, n, stdin);
+    if (ret_val)
+    {
+        find = strchr(st, '\n'); // fine the "\n"
+        if (find)                //if the address is not a NULL
+            *find = '\0';        //put a NULL
+
+        else
+            while (getchar() != '\n')
+                continue; //clean the input cache
+    }
+    return ret_val;
+}

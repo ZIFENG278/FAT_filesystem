a07d66e5999ff47a83a70f4fc40f3b4ece46231f
Zifeng ZHANG <zhang_z9@auditeur.cnam.fr>
2021-12-16 22:58:18 +0100

    add fs_fat.c

---
+++ fs_fat.c
@@ -0,0 +1,73 @@
+#include "oscafs.h"
+
+#define FREE_BLK 0
+#define EOF_BLK -1
+
+// Error Values
+#define NO_FREE_BLK -2
+#define NOT_EOF -3
+
+int fat[NB_BLOCKS];
+
+void init_fat()
+{
+    for (int i = 0; i < NB_BLOCKS; i++)
+        fat[i] = FREE_BLK;
+}
+
+int *get_fat()
+{
+    return fat;
+}
+
+int eof_block(int i)
+{
+    return fat[i] == EOF_BLK;
+}
+
+int next_block(int i)
+{
+    return fat[i];
+}
+
+int first_free_block()
+{
+    for (int i = 1; i < NB_BLOCKS; i++)
+    {
+        if (fat[i] == FREE_BLK)
+        {
+            return i;
+        }
+    }
+    return NO_FREE_BLK;
+}
+
+int alloc_block()
+{
+    int i;
+
+    if ((i = first_free_block()) > 0)
+    {
+        fat[i] = EOF_BLK;
+        return i;
+    }
+
+    return NO_FREE_BLK;
+}
+
+int add_next_block(int b)
+{
+    int i;
+
+    if (fat[b] != EOF_BLK)
+        return NOT_EOF;
+
+    if ((i = first_free_block()) > 0)
+    {
+        fat[b] = i;
+        fat[i] = EOF_BLK;
+        return i;
+    }
+
+    return NO_FREE_BLK;
+}
